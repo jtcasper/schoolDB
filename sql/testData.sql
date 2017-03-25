8bb0ee0f2a9c414381b790b7f50486dfd4de3708
Jacob Casper <jacob.thomas.casper@gmail.com>
2017-03-24 20:49:07 -0400

    Updated testData for login via interactive shell. Waiting on CLOB changes to finish and push interactive shell.

--- sql/testData.sql
+++ sql/testData.sql
@@ -1,7 +1,7 @@
 INSERT ALL
-  INTO ADMINS (eid, pw, fname, lname, ssn) VALUES(1, 'pw', 'john', 'smith', '600-20-4000')
-  INTO ADMINS (eid, pw, fname, lname, ssn) VALUES(2, 'pw', 'john', 'jones', '102-40-5550')
-  INTO ADMINS (eid, pw, fname, lname, ssn) VALUES(3, 'pw', 'gabby', 'smith', '224-43-8091')
-  INTO ADMINS (eid, pw, fname, lname, ssn) VALUES(4, 'pw', 'rita', 'overmore', '601-30-9009')
-  INTO ADMINS (eid, pw, fname, lname, ssn) VALUES(5, 'pw', 'john', 'smith', '600-70-7006')
+  INTO ADMIN (eid, password, fname, lname, ssn) VALUES(1, 'pw', 'john', 'smith', '600204000')
+  INTO ADMIN (eid, password, fname, lname, ssn) VALUES(2, 'pw', 'john', 'jones', '102405550')
+  INTO ADMIN (eid, password, fname, lname, ssn) VALUES(3, 'pw', 'gabby', 'smith', '224438091')
+  INTO ADMIN (eid, password, fname, lname, ssn) VALUES(4, 'pw', 'rita', 'overmore', '601309009')
+  INTO ADMIN (eid, password, fname, lname, ssn) VALUES(5, 'pw', 'john', 'smith', '600707006')
 SELECT 1 FROM DUAL;
